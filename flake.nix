ee8d11987b6577c2c00555be200861c0a6537679
Patrick Ferris <patrick@sirref.org>
2025-04-21 14:13:47 +0100

    Nixxxx

--- flake.nix
+++ flake.nix
@@ -1,34 +1,38 @@
 {
-  inputs = {
-    opam-nix.url = "github:tweag/opam-nix";
-    flake-utils.url = "github:numtide/flake-utils";
-    nixpkgs.follows = "opam-nix/nixpkgs";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+  inputs.opam-nix = {
+    url = "github:tweag/opam-nix";
+    inputs.nixpkgs.follows = "nixpkgs";
+    inputs.flake-utils.follows = "flake-utils";
+    inputs.opam-repository.follows = "opam-repository";
+    inputs.opam-overlays.follows = "opam-overlays";
   };
-  outputs =
-    {
-      self,
-      flake-utils,
-      opam-nix,
-      nixpkgs,
-    }@inputs:
-    # Don't forget to put the package name instead of `throw':
-    let
-      package = "sherlorocq";
-    in
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-        on = opam-nix.lib.${system};
-        scope = on.buildOpamProject { } package ./. { ocaml-base-compiler = "*"; };
-        overlay = final: prev: {
-          # Your overrides go here
-        };
-      in
-      {
-        legacyPackages = scope.overrideScope overlay;
+  inputs.opam-repository = {
+    url = "github:ocaml/opam-repository";
+    flake = false;
+  };
+  inputs.opam-overlays = {
+    url = "github:dune-universe/opam-overlays";
+    flake = false;
+  };
 
-        packages.default = self.legacyPackages.${system}.${package};
-      }
-    );
+  outputs = { self, nixpkgs, opam-nix, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        inherit (opam-nix.lib.${system}) buildOpamProject;
+
+        build_ocaml_package = name: path:
+          let scope = buildOpamProject { } name path { ocaml = "*"; };
+
+          in scope.${name}.overrideAttrs (_: {
+            # Prevent unnecessary dependencies on the resulting derivation
+            removeOcamlReferences = true;
+            doNixSupport = false;
+          });
+
+      in rec {
+        packages = { sherlorocq = build_ocaml_package "sherlorocq" ./.; };
+        defaultPackage = packages.sherlorocq;
+
+      });
 }
